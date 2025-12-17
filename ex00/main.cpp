38cd53169b9b89fc508042d9eea29fdf988caa33
yoshin <yoshin@student.42gyeongsan.kr>
2025-12-17 17:54:29 +0900

    test: ex00 - ClapTrap 기본 기능 종합 테스트 추가
    - 생성자 테스트: 기본, 이름, 복사 생성자
    - 기본 공격 및 데미지 처리 테스트
    - 수리 기능 테스트
    - 에너지 소진 테스트 (10회 공격)
    - 에너지 부족 상태에서 동작 확인
    - 체력 소진 테스트
    - 복사 대입 연산자 테스트
    - 소멸자 호출 순서 확인
    
    각 테스트 섹션을 구분하는 printSeparator 함수 추가로
    가독성 향상

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,26 +6,54 @@
 /*   By: yoshin <yoshin@student.42gyeongsan.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/12 12:49:13 by yoshin            #+#    #+#             */
-/*   Updated: 2025/12/14 19:51:20 by yoshin           ###   ########.fr       */
+/*   Updated: 2025/12/17 17:45:37 by yoshin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
+#include <iostream>
+
+static void printSeparator(const std::string& title) {
+  std::cout << "\n========== " << title << " ==========\n" << std::endl;
+}
 
 int main(void) {
-  ClapTrap clap1("CT-1");
-  ClapTrap clap2("CT-2");
+  printSeparator("생성자 테스트");
+  ClapTrap clap1("Warrior");
+  ClapTrap clap2("Mage");
   ClapTrap clap3;
+  ClapTrap clap4(clap1);
 
-  clap1.attack("CT-2");
+  printSeparator("기본 공격 테스트");
+  clap1.attack("Mage");
   clap2.takeDamage(0);
+  clap2.attack("Warrior");
+
+  printSeparator("수리 테스트");
   clap2.beRepaired(5);
-  clap3.attack("CT-1");
+  clap2.beRepaired(3);
 
-  for (int i = 0; i < 10; i++)
-    clap1.attack("target");
-
+  printSeparator("에너지 소진 테스트 (10회 공격)");
+  for (int i = 0; i < 10; i++) {
+    clap1.attack("dummy");
+  }
+  
+  printSeparator("에너지 부족 상태 테스트");
   clap1.attack("target");
+  clap1.beRepaired(10);
 
+  printSeparator("체력 소진 테스트");
+  clap2.takeDamage(10);
+  clap2.takeDamage(5);
+  clap2.attack("target");
+  clap2.beRepaired(5);
+
+  printSeparator("복사 대입 연산자 테스트");
+  ClapTrap clap5("Original");
+  clap5.attack("enemy");
+  ClapTrap clap6("Copy");
+  clap6 = clap5;
+
+  printSeparator("소멸자 테스트 (프로그램 종료 시)");
   return 0;
 }
