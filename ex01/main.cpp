18dfba346dc2461e9c3a921ba5c6a63445808e02
yoshin <yoshin@student.42gyeongsan.kr>
2026-01-14 15:16:41 +0900

    ex01: expand ScavTrap main tests

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -28,32 +28,36 @@ int main(void) {
   clap.attack("enemy");
   std::cout << "\n[ScavTrap의 attack (오버라이드)]" << std::endl;
   scav1.attack("enemy");
+  scav2.attack("enemy");
 
   printSeparator("상속된 메서드 테스트 - takeDamage & beRepaired");
   scav1.takeDamage(30);
   scav1.beRepaired(20);
   scav1.takeDamage(50);
   scav1.beRepaired(10);
+  scav2.takeDamage(30);
+  scav2.beRepaired(20);
+  scav2.takeDamage(50);
+  scav2.beRepaired(10);
 
   printSeparator("ScavTrap 특수 능력 테스트 - guardGate");
   scav1.guardGate();
   scav2.guardGate();
 
   printSeparator("복사 생성자 테스트");
-  ScavTrap scav3(scav1);
-  scav3.attack("clone_target");
-  scav3.guardGate();
-
-  printSeparator("대입 연산자 테스트");
-  ScavTrap scav4("ST-Temp");
-  scav4 = scav1;
-  scav4.attack("assigned_target");
+  ScavTrap scav3("ST-Guard-3");
 
   printSeparator("에너지 소진 테스트 (50회 공격)");
   for (int i = 0; i < 50; i++) {
-    scav1.attack("dummy");
+    scav3.attack("dummy");
   }
-  scav1.attack("no_energy");
+  scav3.attack("no_energy");
+
+  printSeparator("대입 연산자 테스트");
+  ScavTrap scav4("ST-Temp");
+  scav4.status();
+  scav4 = scav3;
+  scav4.status();
 
   printSeparator("체력 소진 테스트");
   ScavTrap scav5("ST-Dying");
